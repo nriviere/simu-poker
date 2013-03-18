184bbc781590b2364038c8ee3fb3f3a6e5a8c899
jobraga <joffray.braga@etud.univ-montp2.fr>
2013-03-18 21:20:25 +0100

    Create MySqlApi.cpp

---
+++ MySqlApi.cpp
@@ -0,0 +1,54 @@
+#include "MySqlApi.h"
+
+MySqlAPI::MySqlAPI(string path, string bdd){
+    string default_param("");
+    mysql_path = path;
+    default_param += " -h localhost -u root --default-character-set=latin1 ";
+    default_param += (bdd + " -e \"");
+    string_param = default_param;
+    file_param = default_param + "source ";
+}
+
+int MySqlAPI::file_req_exec(string sql_file_path, string outfile){
+    int ret = -1;
+
+    //cout << "Insertion depuis " << sql_file_path << "...";
+
+    string cmd = mysql_path + file_param + sql_file_path + "\"";
+
+    if(outfile.length()) cmd += (" > "+ outfile);
+
+    if(system(NULL)) ret = system(cmd.c_str());
+
+    //cout << (ret==0 ? "OK!" : "ERREUR!") << endl;
+    return ret;
+}
+
+int MySqlAPI::string_req_exec(string sql_string, string outfile){
+    int ret = -1;
+    string cmd = mysql_path + string_param + sql_string + "\"";
+
+    if(outfile.length()>0) cmd += (" > "+ outfile);
+
+    //cout << "Execute : \n" << sql_string << "...";
+    if(system(NULL)) ret = system(cmd.c_str());
+    //cout << (ret==0 ? "OK!" : "ERREUR!") << endl;
+    return ret;
+}
+
+int MySqlAPI::rep_exec(string path, string outfile){
+    int ret = 0;
+
+    DIR *rep = opendir(path.c_str());
+    if(rep){
+        struct dirent *cur_file = NULL;
+        while( (cur_file = readdir(rep))!=NULL  && ret==0 ){
+            if(strcmp(cur_file->d_name,".")!=0 && strcmp(cur_file->d_name,"..")!=0){
+                string cur_file_path = path + "/" + cur_file->d_name;
+                ret = file_req_exec(cur_file_path, outfile);
+            }
+        }
+    }
+    else cerr << "Erreur, impossible de trouver : " << path << endl;
+    return ret;
+}
